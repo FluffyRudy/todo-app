5fcc30044fd3485b58218092116d8b1451932894
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-01-15 23:05:55 +0545

    add script to  get userinput for todo

---
+++ src/components/input.js
@@ -0,0 +1,66 @@
+import "../styles/userInput.css";
+import { Widget } from "../share/widgets";
+import { Todo } from "../lib/todo";
+import { Storage } from "../lib/storage";
+
+function displayPopUp() {
+    document.querySelector("dialog").showModal();
+}
+
+function displayErrorPopup(erroMsg) {
+    const popup = document.createElement("dialog");
+    popup.style.width = "min(600px, 100vw)";
+    popup.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
+    document.body.appendChild(popup);
+    const popupContent = Widget.createText(erroMsg);
+    popupContent.style.fontSize = "3em";
+    popupContent.style.fontFamily = "monospace";
+    popupContent.style.color = "white";
+    popupContent.style.textAlign = "center";
+    popup.appendChild(popupContent);
+    popup.showModal();
+    setTimeout(() => {
+        popup.close()
+    }, 2000);
+}
+
+function getUserInputs() {
+    const label = document.getElementById("todo-label");
+    const discription = document.getElementById("todo-disc");
+    const deadline = document.getElementById("todo-deadline");
+    return [label.value, discription.value, deadline.valueAsDate];
+}
+
+function closePopup(e) {
+    e.preventDefault();
+    document.querySelector("dialog").close();
+}
+
+function addTodo(e) {
+    const userInput = getUserInputs();
+    const todoObj = new Todo(...userInput);
+    if (userInput[0].length < 4) {
+        e.preventDefault();
+        displayErrorPopup("Provide proper label");
+        return;
+    } else if (userInput[1].length < 4) {
+        e.preventDefault();
+        displayErrorPopup("Provide proper description");
+        return;
+    }
+    Storage.addToStorage(todoObj);
+    closePopup(e);
+}
+
+export function TodoAddButton() {
+    return Widget.createButton("", "id", "add-button");
+}
+
+export function registerEventListener() {
+    document.getElementById("add-button")
+        .addEventListener('click', displayPopUp);
+    document.getElementById("add-todo")
+        .addEventListener('click', addTodo);
+    document.getElementById("cancel-todo")
+        .addEventListener('click', closePopup);
+}
