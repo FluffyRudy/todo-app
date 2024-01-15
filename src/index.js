7583f80dc80b87a8a1a539d11feaaaf45a0aecf6
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-01-15 23:02:38 +0545

    add todo 'add button'

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import "./styles/main.css";
-import { createHeader } from "./components/header";
-import { Widget } from "./share/widgets";
+import header  from "./components/header";
+import footer from "./components/footer";
+import { TodoAddButton, registerEventListener } from "./components/input";
+import { Storage } from "./lib/storage";
 
 const container = document.getElementById("container");
-const addButton = Widget.createButton("", "id", "add-button");
 
-container.appendChild(createHeader())
-document.body.appendChild(addButton);
+container.appendChild(header())
+container.appendChild(footer());
+container.appendChild(TodoAddButton());
+
+registerEventListener();
