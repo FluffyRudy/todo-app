899460784fd9422d643d43eb69e05911870ffa42
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-01-17 20:18:53 +0545

    add components to proper place

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import "./styles/main.css";
 import header  from "./components/header";
 import footer from "./components/footer";
-import { TodoAddButton, registerEventListener } from "./components/input";
+import { TodoAddButton, registerUserInputListener } from "./components/input";
+import { createMenu, registerMenuEventListers } from "./components/header";
 import { Storage } from "./lib/storage";
 
 const container = document.getElementById("container");
 
-container.appendChild(header())
-container.appendChild(footer());
-container.appendChild(TodoAddButton());
+document.body.insertBefore(TodoAddButton(), container);
+document.body.insertBefore(header(), container)
+document.body.insertBefore(createMenu(), container);
+document.body.appendChild(footer());
 
-registerEventListener();
+
+registerMenuEventListers();
+registerUserInputListener();
